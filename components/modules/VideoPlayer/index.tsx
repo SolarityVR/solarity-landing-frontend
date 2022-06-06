2d86b4ece01cc9aa4b232bbf949dd62e5e5920ed
jl <jamesdream0724@gmail.com>
2022-06-06 15:08:07 +0700

    youtube component --2

--- components/modules/VideoPlayer/index.tsx
+++ components/modules/VideoPlayer/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-// import ReactPlayer from "react-player/youtube";
-import YouTube from "react-youtube";
+import ReactPlayer from "react-player/youtube";
 
 export const VideoPlayer = () => {
   return (
     <>
         <div className="p-[20px] sm:p-[100px] relative">
-            {/* <ReactPlayer
+            <ReactPlayer
                 controls = {true}
                 className='react-player fixed-bottom'
                 url='https://youtu.be/s3ktYTdV6gw'
-            /> */}
-            <YouTube videoId="s3ktYTdV6gw" />
+            />
         </div>
     </>
   );
