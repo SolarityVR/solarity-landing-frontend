441d976f1097b89f8c7e2d1480ccdffeb280401f
jl <jamesdream0724@gmail.com>
2022-06-06 10:00:32 +0700

    faq selection

--- components/common/Accordion.tsx
+++ components/common/Accordion.tsx
@@ -10,9 +10,9 @@ const Accordion = (props: AccordionProps) => {
   const [opened, setOpened] = useState(false);
   return (
     <div className='rounded-2xl bg-gradient-linear p-[24px] my-6'>
-      <div>
+      <div className="cursor-pointer" onClick={() => setOpened(!opened)}>
         <h1 className='text-white text-[16px] sm:text-[26px] mb-1 inline-block'>{props.title}</h1>
-        <i className={"pr-[10px] float-right my-2 text-primary cursor-pointer fa "+(opened?"fa-minus":"fa-plus")} onClick={() => setOpened(!opened)}></i>
+        <i className={"pr-[10px] float-right my-2 text-primary fa "+(opened?"fa-minus":"fa-plus")}></i>
       </div>
       <div className={opened?"":"h-0 leading-0 overflow-hidden"}>
         <br></br>
