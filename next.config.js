cc2946d0df5927b1291445c3716cdc718dc4c2f1
jamesl0724 <jamesdream0724@gmail.com>
2022-06-22 09:38:53 +0700

    next config for aws

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-    distDir: 'build',
-    exportPathMap: async function(
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-    ) {
-        return {
-          "/": { page: "/" }
-        };
-    },
-    images: {
-        loader: 'imgix',
-        path: 'https://example.com/myaccount/',
-    },
+    reactStrictMode: false
 }
