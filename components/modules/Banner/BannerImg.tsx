79e1b7b8a0a76b3ddfa3eb7315a3687f17fe0e81
jamesl0724 <jamesdream0724@gmail.com>
2022-06-23 11:16:18 +0700

    ball image

--- components/modules/Banner/BannerImg.tsx
+++ components/modules/Banner/BannerImg.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import BannerImg from "../../../assets/images/bannerImg.png";
+import BannerImg from "../../../assets/images/bannerImg.webp";
 
 export const BannerImage = () => {
   return (
