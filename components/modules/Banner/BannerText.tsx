64cc5be3d1ff2e281d15f2c3738796dfaf395e77
Dream <99065080+jamesl0724@users.noreply.github.com>
2022-07-21 09:12:38 +0700

    Update BannerText.tsx

--- components/modules/Banner/BannerText.tsx
+++ components/modules/Banner/BannerText.tsx
@@ -9,11 +9,8 @@ export const BannerText = () => {
     <>
         <div className="my-5 relative text-center sm:text-left">
             <div className="absolute w-[400px] h-[400px] -top-[180px] -left-[200px] -z-50 bg-gradient-radial from-purple-500 via-pink-500 to-red-500"></div>
-            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeIn">
               <span className="text-white text-[26px] md:text-5xl font-semibold tracking-wider leading-[2.7rem] md:leading-[3.7rem]">Solarity, the framework <br /> for your metaverse</span>
-            </AnimationOnScroll>
         </div>
-        <AnimationOnScroll animateOnce={true} animateIn="animate__fadeIn">
           <div className="my-5 mb-[56px] text-center sm:text-left">
               <span className="text-content text-[14px] md:text-[22px]">Building, accelerating and powering <br /> the metaverse, one DAO at a time.</span>
           </div>
@@ -21,7 +18,6 @@ export const BannerText = () => {
               <div className="pr-5 mb-5"><Button caption="Start App" icon="" bordered={false} onClick={() => router.push('https://solarity-stage.vercel.app')} /></div>
               <div className="pr-5 xl:pr-[30%] col-span-2"><Button caption="Download extension" icon="chrome" bordered={true} onClick={() => router.push('https://chrome.google.com/webstore/detail/twitter-verse/olmkofkehgfecicmijdhohcgglpnbnnh')} /></div>
           </div>
-        </AnimationOnScroll>
     </>
   );
 };
