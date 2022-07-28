08dbf2796257e9323a831da36fa02f48a64f3f31
Dream <99065080+jamesl0724@users.noreply.github.com>
2022-07-28 07:31:26 +0700

    Update BannerText.tsx

--- components/modules/Banner/BannerText.tsx
+++ components/modules/Banner/BannerText.tsx
@@ -9,10 +9,10 @@ export const BannerText = () => {
     <>
         <div className="my-5 relative text-center sm:text-left">
             <div className="absolute w-[400px] h-[400px] -top-[180px] -left-[200px] -z-50 bg-gradient-radial from-purple-500 via-pink-500 to-red-500"></div>
-              <span className="text-white text-[26px] md:text-5xl font-semibold tracking-wider leading-[2.7rem] md:leading-[3.7rem]">Solarity, the framework <br /> for your metaverse</span>
+              <span className="text-white text-[26px] md:text-5xl font-semibold tracking-wider leading-[2.7rem] md:leading-[3.7rem]">Solarity, Your Social <br /> GameFi Aggregator</span>
         </div>
           <div className="my-5 mb-[56px] text-center sm:text-left">
-              <span className="text-content text-[14px] md:text-[22px]">Building, accelerating and powering <br /> the metaverse, one DAO at a time.</span>
+              <span className="text-content text-[14px] md:text-[22px]">Your Games, Your Friends <br /> Your Quests/Trophies, Anywhere</span>
           </div>
           <div className="grid grid-cols-1 xl:grid-cols-3">
               <div className="pr-5 mb-5"><Button caption="Start App" icon="" bordered={false} onClick={() => router.push('https://solarity-stage.vercel.app')} /></div>
