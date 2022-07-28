5dd4420f7e22a8cbda8cb7692aac000dd650742a
Dream <99065080+jamesl0724@users.noreply.github.com>
2022-07-28 07:32:34 +0700

    Update BannerText.tsx

--- components/modules/Banner/BannerText.tsx
+++ components/modules/Banner/BannerText.tsx
@@ -12,7 +12,7 @@ export const BannerText = () => {
               <span className="text-white text-[26px] md:text-5xl font-semibold tracking-wider leading-[2.7rem] md:leading-[3.7rem]">Solarity, Your Social <br /> GameFi Aggregator</span>
         </div>
           <div className="my-5 mb-[56px] text-center sm:text-left">
-              <span className="text-content text-[14px] md:text-[22px]">Your Games, Your Friends <br /> Your Quests/Trophies, Anywhere</span>
+              <span className="text-content text-[14px] md:text-[22px]">Your Games, Your Friends <br /> Your Quests&#47;Trophies, Anywhere</span>
           </div>
           <div className="grid grid-cols-1 xl:grid-cols-3">
               <div className="pr-5 mb-5"><Button caption="Start App" icon="" bordered={false} onClick={() => router.push('https://solarity-stage.vercel.app')} /></div>
