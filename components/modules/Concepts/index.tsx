fb2b9e6d55e5db0c38d05a18a582cfb6a40ec47a
Dream <99065080+dreamhi724@users.noreply.github.com>
2022-08-01 08:20:15 +0700

    Update index.tsx

--- components/modules/Concepts/index.tsx
+++ components/modules/Concepts/index.tsx
@@ -28,7 +28,7 @@ export const Concepts = () => {
             <AnimationOnScroll animateOnce={true} animateIn="animate__fade">
                 <div className="my-15 mt-[36px] grid grid-cols-5">
                     <div className="col-span-5 text-center sm:col-span-4 sm:text-left">
-                        <span className="text-content text-[14px] sm:text-[22px]">Solarity is an autopoietic modular metaverse that consists of many applications built on top of a new standard of crypto-economic systems to accelerate blockchain adoption by increasing productivity and efficiency for any kind of organization and to assist builders embarking on the web3 journey.</span>
+                        <span className="text-content text-[14px] sm:text-[22px]">{"Solarity is the framework to give the order web3 Gaming deserves. Who knows how to build good games usually isn’t efficient at web3 marketing and blockchain development, and viceversa for web3 communities and devs leading to almost no active users. We want to provide a place for games to let any user access their game anywhere, and the right incentives for users to play it and share it."}</span>
                     </div>
                 </div>
             </AnimationOnScroll>
@@ -36,14 +36,14 @@ export const Concepts = () => {
                 <div className="col-span-5 text-left lg:col-span-4 pr-[20px]">
                     <div className="grid grid-cols-1 sm:grid-cols-2">
                         <div className="pr-[1rem] sm:pr-[8rem] mb-14">
-                            <ConceptCard image={Concept1} title="Create" description="Create your presence in the metaverse for the cheapest possible price, whether it’s NFTs or DAOs." />
+                            <ConceptCard image={Concept1} title="Create" description="Create your presence in the metaverse for the cheapest possible price, whether it’s NFTs, Games or DAOs." />
                         </div>
                         <div className="pr-[1rem] sm:pr-[8rem] mb-14">
-                            <ConceptCard image={Concept2} title="Connect" description="Establish symbiotic relationships in open ecosystems to compound network effects to extents that Web2 walled gardens cannot compete with." />
+                            <ConceptCard image={Concept2} title="Connect" description="Share your experiences and invite your friends anywhere on the web, on any platform with just one link directly from the web2 accounts you use daily." />
                         </div>
                         <div className="pr-[1rem] sm:pr-[8rem] mb-14 relative">
                             <div className="absolute w-[400px] h-[400px] -top-[180px] -left-[200px] -z-50 bg-light-gradient-radial from-purple-500 via-pink-500 to-red-500"></div>
-                            <ConceptCard image={Concept3} title="Cultivate" description="Grow your project with your community through flawless continuous funding for DAOs and native recursive sub DAOs to create unseen organizational structures that can scale without compromising on decentralization, efficiency and transparency." />
+                            <ConceptCard image={Concept3} title="Cultivate" description="Grow your project and your community through flawless user interactions thanks to our GameFi aggregator. Devs focus on building the best games you can, and users on having fun and earning by browsing the web." />
                         </div>
                         <div className="pr-[1rem] sm:pr-[8rem] mb-14">
                             <ConceptCard image={Concept4} title="Compose" description="Have complete freedom in mixing ingredients to create new components for the systems of the future, welcome to the exponential age." />
